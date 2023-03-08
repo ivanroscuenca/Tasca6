a6c5097c1b98b376374e8aa401834b146cf3eee8
ivanroscuenca <ivanros@protonmail.com>
2023-03-08 16:19:02 +0100

    Tasca 6 realizada update 3

--- src/N1ex1/Main.java
+++ src/N1ex1/Main.java
@@ -3,9 +3,17 @@ package N1ex1;
 public class Main {
 
     public static void main(String[] args) {
-        Car seat = new Car("Ibiza");
-        Car fiat = new Car("tipo");
-        Car volkswagen = new Car("golf");
+String n1 ="Ivan";
+String n2 = "Emma";
+String n3 = "Jessica";
+
+//Call to method
+
+        NoGenericMethods gen1 = new NoGenericMethods(n3,n2,n1);
+        NoGenericMethods gen2 = new NoGenericMethods(n1,n2,n3);
+        NoGenericMethods gen3 = new NoGenericMethods(n2,n3,n1);
+//print
+        System.out.println(gen1 + "\n" + gen2 + "\n" + gen3 );
 
     }
 }
--- src/N1ex1/NoGenericMethods.java
+++ src/N1ex1/NoGenericMethods.java
@@ -4,19 +4,52 @@ import org.w3c.dom.ls.LSOutput;
 
 public class NoGenericMethods {
 
-//    Crea una classe anomenada NoGenericMethods que emmagatzemi tres objectes del
+    //    Crea una classe anomenada NoGenericMethods que emmagatzemi tres objectes del
 //    mateix tipus, juntament amb els mètodes per a emmagatzemar i extreure aquests objectes i un constructor per
 //    a inicialitzar els tres. Comprova que els arguments es poden posar en qualsevol posició en la crida al
 //    constructor.
-    Car Car1;
-    Car car2;
-    Car car3;
 
-    public String NoGenericMethods(Car car1, Car car2, Car car3) {
-        this.Car1 = car1;
-        this.car2 = car2;
-        this.car3 = car3;
+    private String n1;
+    private String n2;
+    private String n3;
 
-        return car1.getNom()+car2.getNom()+car3.getNom();
+    public NoGenericMethods(String n1, String n2, String n3) {
+        this.n1 = n1;
+        this.n2 = n2;
+        this.n3 = n3;
+    }
+
+    public String getN1() {
+        return n1;
+    }
+
+    public void setN1(String n1) {
+        this.n1 = n1;
+    }
+
+    public String getN2() {
+        return n2;
+    }
+
+    public void setN2(String n2) {
+        this.n2 = n2;
+    }
+
+    public String getN3() {
+        return n3;
+    }
+
+    public void setN3(String n3) {
+        this.n3 = n3;
+    }
+
+    @Override
+    public String toString() {
+        return "NoGenericMethods{" +
+                "n1='" + n1 + '\'' +
+                ", n2='" + n2 + '\'' +
+                ", n3='" + n3 + '\'' +
+                '}';
     }
 }
+
